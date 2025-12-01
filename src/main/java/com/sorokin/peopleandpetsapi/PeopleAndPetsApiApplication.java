c9d3e46333724d5b38956ec1d722315b205826ee
Alexey Berasneu <alexeyberasneu@gmail.com>
2025-11-30 21:17:09 -0500

    initial project

---
+++ src/main/java/com/sorokin/peopleandpetsapi/PeopleAndPetsApiApplication.java
@@ -0,0 +1,13 @@
+package com.sorokin.peopleandpetsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleAndPetsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeopleAndPetsApiApplication.class, args);
+    }
+
+}
